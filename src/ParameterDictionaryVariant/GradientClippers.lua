010f19781b508360969ee6254b0c7a0392974ac2
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-11-12 15:15:50 +0800

    Create GradientClippers.lua

---
+++ src/ParameterDictionaryVariant/GradientClippers.lua
@@ -0,0 +1,151 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Deep Learning Library (DataPredict Axon)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict-Axon/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local AqwamTensorLibrary = require(script.Parent.AqwamTensorLibraryLinker.Value)
+
+local DisplayErrorFunctions = require(script.Parent.DisplayErrorFunctions)
+
+local displayFunctionErrorDueToNonObjectCondition = DisplayErrorFunctions.displayFunctionErrorDueToNonObjectCondition
+
+local GradientClipper = {}
+
+GradientClipper.__index = GradientClipper
+
+local defaultDecayRate = 0.5
+
+function GradientClipper.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local NewGradientClipper = {}
+
+	setmetatable(NewGradientClipper, GradientClipper)
+	
+	NewGradientClipper.ClipFunction = parameterDictionary.CalculateFunction or parameterDictionary[1]
+	
+	NewGradientClipper.Optimizer = parameterDictionary.Optimizer or parameterDictionary[2]
+	
+	NewGradientClipper.isAnObject = true
+	
+	return NewGradientClipper
+	
+end
+
+function GradientClipper.ClipValue(parameterDictionary)
+
+	parameterDictionary = parameterDictionary or {}
+
+	local minimumValue = parameterDictionary.minimumValue or parameterDictionary[1] or -1
+	
+	local maximumValue = parameterDictionary.maximumValue or parameterDictionary[2] or 1
+	
+	local Optimizer = parameterDictionary.Optimizer or parameterDictionary[3]
+	
+	local functionToApply = function(value) return math.clamp(value, minimumValue, maximumValue) end
+
+	local ClipFunction = function(firstDerivativeTensor) return AqwamTensorLibrary:applyFunction(functionToApply, firstDerivativeTensor) end
+
+	return GradientClipper.new({ClipFunction, Optimizer}) 
+
+end
+
+function GradientClipper.ClipNormalization(parameterDictionary)
+
+	parameterDictionary = parameterDictionary or {}
+
+	local normalizationValue = parameterDictionary.normalizationValue or parameterDictionary[1] or 2
+
+	local maximumNormalizationValue = parameterDictionary.maximumNormalizationValue or parameterDictionary[2] or normalizationValue
+
+	local Optimizer = parameterDictionary.Optimizer or parameterDictionary[3]
+
+	local ClipFunction = function(firstDerivativeTensor) 
+
+		local squaredFirstDerivativeTensor = AqwamTensorLibrary:power(firstDerivativeTensor, normalizationValue)
+
+		local sumSquaredFirstDerivativeTensor = AqwamTensorLibrary:sum(squaredFirstDerivativeTensor)
+
+		local currentNormalizationValue = math.pow(sumSquaredFirstDerivativeTensor, (1 / normalizationValue))
+
+		if (currentNormalizationValue ~= 0) then
+
+			firstDerivativeTensor = AqwamTensorLibrary:multiply(firstDerivativeTensor, (maximumNormalizationValue / currentNormalizationValue))
+
+		end
+
+		return firstDerivativeTensor
+		
+	end
+
+	return GradientClipper.new({ClipFunction, Optimizer}) 
+
+end
+
+function GradientClipper:calculate(parameterDictionary)
+	
+	displayFunctionErrorDueToNonObjectCondition(not self.isAnObject)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local learningRate = parameterDictionary.learningRate or parameterDictionary[1]
+
+	local firstDerivativeTensor = parameterDictionary.firstDerivativeTensor or parameterDictionary[2]
+
+	local ClipFunction = self.ClipFunction
+	
+	local Optimizer = self.Optimizer
+	
+	if (not ClipFunction) then error("No calculate function.") end
+	
+	firstDerivativeTensor = ClipFunction(firstDerivativeTensor)
+	
+	if (Optimizer) then
+		
+		firstDerivativeTensor = Optimizer:calculate(parameterDictionary)
+		
+	else
+		
+		firstDerivativeTensor = AqwamTensorLibrary:multiply(learningRate, firstDerivativeTensor)
+		
+	end
+	
+	return firstDerivativeTensor
+	
+end
+
+function GradientClipper:reset()
+	
+	displayFunctionErrorDueToNonObjectCondition(not self.isAnObject)
+	
+	local Optimizer = self.Optimizer
+	
+	if (Optimizer) then Optimizer:reset() end
+	
+end
+
+return GradientClipper
